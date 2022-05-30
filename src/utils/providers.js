650db1e8b6b1ab89aaf023af3bdfc9423c4daf0b
igorkishko <kishko.igor@gmail.com>
2022-05-30 16:42:21 +0300

    changed Ropsten to Sepolia

--- src/utils/providers.js
+++ src/utils/providers.js
@@ -3,21 +3,25 @@ import { ethers } from 'ethers';
 const {
   REACT_APP_ETH_CHAIN_ID,
   REACT_APP_AMB_CHAIN_ID,
-  REACT_APP_AMB_RPC_URL,
   REACT_APP_INFURA_KEY,
+  REACT_APP_AMB_RPC_URL,
+  REACT_APP_ETH_WS_RPC_URL,
 } = process.env;
 
+// ethereum read-only provider configuration
 export const ethChainId = +REACT_APP_ETH_CHAIN_ID;
 
-const ethProvider = new ethers.providers.InfuraProvider(
-  ethChainId,
-  REACT_APP_INFURA_KEY,
+const ethProvider = new ethers.providers.WebSocketProvider(
+    REACT_APP_ETH_WS_RPC_URL + REACT_APP_INFURA_KEY,
+    ethChainId,
 );
 
+// ambrosus read-only provider configuration
 export const ambChainId = +REACT_APP_AMB_CHAIN_ID;
 
-export const ambProvider = new ethers.providers.JsonRpcProvider(
-  REACT_APP_AMB_RPC_URL,
+export const ambProvider = new ethers.providers.StaticJsonRpcProvider(
+    REACT_APP_AMB_RPC_URL,
+    ambChainId,
 );
 
 const providers = {
