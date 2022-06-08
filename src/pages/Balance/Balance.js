52e11a9183ec220866f4a4e340fd9959deb7b828
dimavoronin <zveruha1109>
2022-06-08 10:30:17 +0300

    changed balances

--- src/pages/Balance/Balance.js
+++ src/pages/Balance/Balance.js
@@ -11,7 +11,7 @@ const tableHeads = ['Token name', 'Ambrosus', 'Ethereum', 'Binance Smart Chain']
 const Balance = () => {
   const [balances, setBalances] = useState(null);
 
-  useEffect(async () => {
+  useEffect(() => {
     handleBalances();
   }, []);
 
@@ -21,12 +21,7 @@ const Balance = () => {
     const sambInAmb = getTokenBalance(providers[ambChainId], config.tokens.SAMB.addresses.amb, ambContractAddress);
     const wethInAmb = getTokenBalance(providers[ambChainId], config.tokens.WETH.addresses.amb, ambContractAddress);
 
-    const ethInEth = providers[ethChainId].getBalance(ethContractAddress);
-    const ambInEth = providers[ethChainId].getBalance(ambContractAddress);
-    const ethInAmb = providers[ambChainId].getBalance(ethContractAddress);
-    const ambInAmb = providers[ambChainId].getBalance(ambContractAddress);
-
-    Promise.all([sambInEth, wethInEth, sambInAmb, wethInAmb, ethInEth, ambInEth, ethInAmb, ambInAmb])
+    Promise.all([sambInEth, wethInEth, sambInAmb, wethInAmb])
       .then((res) => {
         setBalances(res);
       })
@@ -45,21 +40,9 @@ const Balance = () => {
           </TableHead>
           <TableBody>
             <TableRow>
-              <TableCell>AMB</TableCell>
-              <TableCell>{utils.formatUnits(balances[7], 18)}</TableCell>
-              <TableCell>{utils.formatUnits(balances[5], 18)}</TableCell>
-              <TableCell>-</TableCell>
-            </TableRow>
-            <TableRow>
               <TableCell>sAMB</TableCell>
               <TableCell>{utils.formatUnits(balances[2], 18)}</TableCell>
               <TableCell>{utils.formatUnits(balances[0], 18)}</TableCell>
-              <TableCell>-</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>ETH</TableCell>
-              <TableCell>{utils.formatUnits(balances[6], 18)}</TableCell>
-              <TableCell>{utils.formatUnits(balances[4], 18)}</TableCell>
               <TableCell>-</TableCell>
             </TableRow>
             <TableRow>
