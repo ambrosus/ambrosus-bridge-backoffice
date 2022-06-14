7c65c7171d660918b51d6e4005824e26a5990218
igorkishko <kishko.igor@gmail.com>
2022-06-14 06:02:15 +0300

    login via metamask

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Home from './pages/Home';
 import Balance from './pages/Balance';
 import Layout from './layouts/Layout';
+import Login from "./pages/Login";
 
 const routes = [
   {
     path: '/',
     key: 'ROOT',
     exact: true,
+    component: () => <Login/>,
+  },
+  {
+    path: '/dashboard',
+    key: 'ROOT',
+    exact: true,
+    protectedRoute: true,
     component: () => <Home />,
   },
   {
     path: '/balance',
     key: 'ROOT',
     exact: true,
+    protectedRoute: true,
     component: () => (
       <Layout>
         <Balance />
