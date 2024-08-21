afc65ef1e9fea5641c9f26b55042f405edd976ea
igorkishko <kishko.igor@gmail.com>
2024-08-21 13:40:50 +0300

    add error logging

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,10 +2,13 @@ import { Button } from "@mui/material";
 import {Buffer} from "buffer";
 import {recoverPersonalSignature} from "eth-sig-util";
 import {useHistory} from "react-router";
+import {useState} from "react";
 
 const Login = () => {
 
 	const history = useHistory();
+
+	const [error, setError] = useState('');
 
 	const verifyLogin = async () => {
 
@@ -13,36 +16,46 @@ const Login = () => {
 
 		const exampleMessage = 'Hello, you are logging in Ambrosus Bridge Backoffice \n\nHave a great day!';
 
-		if (window.ethereum) {
+		if (!window.ethereum) {
+			setError('Please install MetaMask');
+			return;
+		}
+
+		try {
 			const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 			const account = accounts[0];
 
-			try {
-				const from = account
-				const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
-				const sign = await ethereum.request({
-					method: 'personal_sign',
-					params: [msg, from, 'Example password'],
-				});
+			const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
+			const sign = await ethereum.request({
+				method: 'personal_sign',
+				params: [msg, account, 'Example password'],
+			});
 
-				const recoveredAddr = recoverPersonalSignature({
-					data: msg,
-					sig: sign,
-				});
-				
-				if (verifiedAccounts.includes(recoveredAddr)) {
-					document.cookie = 'backoffice verified;max-age=max-age-in-seconds=86400'
-					history.push('/dashboard')
-				}
+			const recoveredAddr = recoverPersonalSignature({
+				data: msg,
+				sig: sign,
+			});
 
-			} catch (err) {
-				console.error(err);
+			if (!verifiedAccounts.includes(recoveredAddr)) {
+				setError('Unauthorized account');
+				return;
 			}
+
+			history.push('/dashboard');
+
+		} catch (err) {
+			console.error(err);
+			const errorString = (JSON.stringify({...err}, null, 2));
+			setError(errorString)
 		}
+
 	}
 
 	return (<section className="login">
 		<Button onClick={verifyLogin} color="primary" size="large" variant="outlined">Login</Button>
+		<div className="error">
+			{error}
+		</div>
 	</section>);
 }
 
